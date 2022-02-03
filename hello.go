83f4b65bfd6441688978440b562acebbe1fbd616
Malabika Sen <malabika1310@gmail.com>
2022-02-03 11:05:10 -0500

    Refactor function to a private function

--- hello.go
+++ hello.go
@@ -8,13 +8,12 @@ const greetingPrefixEnglish = "Hello, "
 const greetingPrefixSpanish = "Hola, "
 const greetingPrefixFrench = "Bonjour, "
 
-func PrintHello(name string, language string) string {
-
-	if name == "" {
-		name = "World"
-	}
-
-	var prefix string
+// greetingPrefix is a private function
+// hence name starts with lowercase letters
+// we do not want the internals of the algorithm to be exposed hence
+// Public functions are uppercase
+// prefix is named return value
+func greetingPrefix(language string) (prefix string) {
 
 	switch language {
 	case spanish:
@@ -25,8 +24,15 @@ func PrintHello(name string, language string) string {
 		prefix = greetingPrefixEnglish
 	}
 
-	return prefix + name
+	return
+}
 
+func PrintHello(name string, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
 }
 func main() {
 	fmt.Println(PrintHello("Malabika", ""))
